c618c0924d3877ddcae630460c0fbd111bd80bfa
Ishan Show <105852558+ishanshow@users.noreply.github.com>
2024-07-28 19:44:36 +0530

    Update iot-irrigation.ino

--- iot-irrigation.ino
+++ iot-irrigation.ino
@@ -2,27 +2,30 @@ const int SENSOR_PIN = 13;
 const int MOTOR_PIN = 4;
 int prev_state = LOW;  // The previous state from the input pin
 int touch_state;     // The current reading from the input pin
+int flag=0;
 
 void setup() {
   // Initialize the Serial to communicate with the Serial Monitor.
   Serial.begin(115200);
   // initialize the ESP8266 NodeMCU's pin as an input
   pinMode(SENSOR_PIN, INPUT);
-  // initialize the Relay Module's pin as an output
   pinMode(MOTOR_PIN, OUTPUT); 
 }
 
 void loop() {
   // read the state of the the input pin:
+  
   touch_state = digitalRead(SENSOR_PIN);
-  if(prev_state == LOW && touch_state == HIGH) {
-    Serial.println("The sensor is touched");
+  if(touch_state == HIGH && flag==0) {
+    Serial.println("Motor switched on");
+    flag=1;
     digitalWrite(MOTOR_PIN, HIGH); 
-      }
-  else if(prev_state == HIGH && touch_state == LOW) {
-    Serial.println("The sensor is released");
-    digitalWrite(MOTOR_PIN, LOW);
+    delay(1000);
   }
-  // save the the last state
-  prev_state = touch_state;
+  if(digitalRead(SENSOR_PIN)==HIGH && flag==1) {
+    digitalWrite(MOTOR_PIN, LOW); 
+    flag=0;
+    Serial.println("Motor switched off");
+    delay(1000);
+  }
 }
