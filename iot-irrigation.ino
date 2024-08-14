27a90f3bad60a19e7aa6a7a0db00685167acd960
Ishan Show <105852558+ishanshow@users.noreply.github.com>
2024-08-14 23:56:40 +0530

    Update iot-irrigation.ino

--- iot-irrigation.ino
+++ iot-irrigation.ino
@@ -16,7 +16,7 @@ void setup() {
 }
 
 void loop() {
-  // read the state of the the input pin:
+  // Read the state of the the input pin:
   touch_state = digitalRead(SENSOR_PIN);
   if(touch_state == HIGH && is_on==0) {
     digitalWrite(MOTOR_PIN, HIGH); 
